967cc3c1be79a687aed25a05b99e087ac3c77124
rernesto <rernesto.espinosa@gmail.com>
2019-06-10 22:22:58 -0400

    Added stopwords list for english and spanish. Fixed SQLite databases URLs.

---
+++ src/Command/BibleTextImportCommand.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Command;
+
+
+use App\Database\DBAL\ConnectionFactory;
+use App\Document\BibleVerse;
+use App\Document\BibleVersion;
+use Doctrine\DBAL\DBALException;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\MongoDBException;
+use Phpml\Tokenization\WordTokenizer;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class BibleTextImportCommand extends Command
+{
+    protected static $defaultName = 'bible:text:import';
+
+    protected $dbalConnectionFactory;
+
+    /**
+     * @var DocumentManager
+     */
+    protected $dm;
+
+    public function __construct(?string $name = null,
+                                ConnectionFactory $dbalConnectionFactory,
+                                DocumentManager $dm, ParameterBagInterface $params
+    )
+    {
+        $this->dbalConnectionFactory = $dbalConnectionFactory;
+        $this->dm = $dm;
+
+        parent::__construct($name);
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Import Bible text from data source.')
+            ->addArgument(
+                'datasource', InputArgument::REQUIRED,
+                'Data source name SpaRVG, SpaRV1865, KJ2000, etc.'
+            )
+            ->addOption(
+                'truncate', null,InputOption::VALUE_OPTIONAL,
+                'Truncate Vocabulary Collection.', false
+            )
+
+        ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     * @throws DBALException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $dbalConnection = $this->dbalConnectionFactory->getConnection(
+            'db' . $input->getArgument('datasource')
+        );
+
+        if($input->getOption('truncate') == true) {
+            $io->warning(sprintf('Truncating Bible Verses Table %s', 'bible_verses'));
+            try {
+//                $databaseUtils->truncateTable('bible_verses');
+            } catch (DBALException $e) {
+                $io->error($e->getMessage());
+            }
+        }
+
+        if (isset($dbalConnection)) {
+            $bibleTextQueryBuilder = $dbalConnection->createQueryBuilder();
+
+
+            $bibleTextQueryBuilder->select('b.*')
+                ->from('bible', 'b');
+
+            $bibleVerses = $bibleTextQueryBuilder->execute()
+                ->fetchAll(\PDO::FETCH_OBJ);
+
+            $io->text(sprintf('Processing %d verses...', count($bibleVerses)));
+            $io->progressStart((int) count($bibleVerses));
+
+            $i = 0;
+            $wordTokenizer = new WordTokenizer();
+
+            $bibleVersionDocument = $this->dm->getRepository(BibleVersion::class)
+                ->findOneBy(['shortName' => $input->getArgument('datasource')]);
+
+            foreach($bibleVerses as $bibleVerse) {
+
+                $bibleVerseDocument = new BibleVerse();
+                $bibleVerseDocument->setReference($bibleVerse->ref);
+                $bibleVerseDocument->setVerseText(
+                    addslashes(preg_replace('/\n/', '', $bibleVerse->verse))
+                );
+                $bibleVerseDocument->setVerseTokens(
+                    implode(
+                        " ",
+                        array_map(
+                            'mb_strtolower',
+                            $wordTokenizer->tokenize($bibleVerseDocument->getVerseText())
+                        )
+                    )
+                );
+                $bibleVerseDocument->setBibleVersion($bibleVersionDocument);
+                $this->dm->persist($bibleVerseDocument);
+                $i++;
+                $io->progressAdvance();
+            }
+            $io->progressFinish();
+
+            $io->text(sprintf('Commiting %d documents to collection...', $i));
+
+            try {
+                $this->dm->flush();
+                $io->success(sprintf('Commited %d documents to collection...', $i));
+            } catch (MongoDBException $e) {
+                $io->error($e->getMessage());
+            }
+        }
+
+        $io->success('Datasource imported successfully');
+    }
+}
